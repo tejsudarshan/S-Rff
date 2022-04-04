cf5a14b45aac70b7c21204f7e5f001f23299843f
Tejas Sudarshan <59815659+tejsudarshan@users.noreply.github.com>
2022-04-04 22:02:04 +0530

    Create test.v

---
+++ test.v
@@ -0,0 +1,20 @@
+module test();
+
+reg clk=0;
+reg s=0;
+reg r=0;
+wire q,qbar;
+
+SRff DUT(s,r,clk,q,qbar);
+
+initial begin
+$monitor($time,"s=%b r=%b q=%b qbar=%b",s,r,q,qbar);
+#5 s=0;r=1;
+#5 s=1;r=0;
+#5 s=1;r=1;
+#5 s=0;r=0;
+#5;
+end
+
+always #1 clk=~clk;
+endmodule
